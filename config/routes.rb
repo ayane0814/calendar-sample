5f988d390d4b37985fedb9de5a5a33089d2d59b9
AyaneNishikawa <149765130+ayane0814@users.noreply.github.com>
2024-05-10 13:12:25 +0000

    Add カレンダー機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root to: "events#index"
+  get '/events', to: 'events#index', defaults: { format: 'json' }
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
